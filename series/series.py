fc91125a15961b14f663d172894d9b4b590aacd2
11mones <mones_leader@hotmail.com>
2023-05-13 14:25:23 +0000

    added the docstring

--- series/series.py
+++ series/series.py
@@ -1,4 +1,5 @@
 def Fibonacci(n):
+    '''The fibonacci function is a function that gives you the nth element of fibonacci series, n is the input of the function'''
 
 
     if n == 0:
@@ -11,9 +12,12 @@ def Fibonacci(n):
         return Fibonacci(n-1) + Fibonacci(n-2)
 
 
+print(Fibonacci.__doc__)
+
 
 
 def lucas(n):
+    '''The lucas function is a function that gives you the nth element of lucas series, n is the input of the function'''
 
     if n == 0:
         return 2
@@ -21,9 +25,13 @@ def lucas(n):
         return 1
 
     return lucas(n-1) + lucas(n-2)
-
+print(lucas.__doc__)
 
 def sum_series(w , x = 0, y = 1):
+    '''The sum series function is a function that takes 3 inputs and two of them are optional, the first input is the nth element
+    of the series, and the other inputs is to determine the base of the series that we are creating, and these two inputs are optional 
+    if you did not pass it to the function, it will act like a fibonacci series
+    '''
 
     if x == 0 and y == 1 :
         if w == 0:
@@ -57,3 +65,4 @@ def sum_series(w , x = 0, y = 1):
     return res[w]
 
 
+print(sum_series.__doc__)
